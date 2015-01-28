e51ef99ca9b56a92f5f898732f6f512878f19f91
Abe Voelker <abe@abevoelker.com>
2015-01-28 12:01:30 -0600

    Initial commit

---
+++ 2/2.scala
@@ -0,0 +1,30 @@
+// Exercise 2.1
+def fib(n: Int): Int = {
+  @annotation.tailrec
+  def loop(n: Int, current: Int, last: Int): Int = {
+    if (n == 0) {
+      last
+    } else {
+      loop(n-1, current + last, current)
+    }
+  }
+  loop(n, 1, 0)
+}
+
+// Exercise 2.2
+// test using e.g.:
+// isSorted(Array(1,2,3), (x: Int, y: Int) => x <= y)
+// isSorted(Array(1,2,1), (x: Int, y: Int) => x <= y)
+// isSorted(Array('a','b','c'), (x: Char, y: Char) => x <= y)
+// isSorted(Array('a','b','a'), (x: Char, y: Char) => x <= y)
+def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+  @annotation.tailrec
+  def loop(n: Int): Boolean =
+    if (n + 1 > as.length - 1) {
+      true
+    } else {
+      if (ordered(as(n), as(n+1))) loop(n + 1)
+      else false
+    }
+  loop(0)
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+My crappy exercise solutions for the
+[Functional Programming in Scala][fp-in-scala] book.
+
+[fp-in-scala]: http://www.manning.com/bjarnason/
